5704bf453d821b1423e787c9daa207b5f588c826
Merijn Spee <merijn.spee@gmail.com>
2025-07-04 20:26:36 +0200

    full portfolio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 
+	import NavBar from '../components/NavBar.svelte';
+
 	let { children } = $props();
 </script>
+
+<NavBar />
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts"></script>
+
+<div class="min-h-screen overflow-auto bg-gradient-to-b from-[#BEE9E8] to-[#CAE9FF] px-4 py-12">
+	<div class="mx-auto max-w-3xl space-y-6 rounded-2xl bg-white p-10 text-[#1B4965] shadow-2xl">
+		<h1 class="text-center text-4xl font-bold">Talentenpaspoort PD4</h1>
+		<p class="text-lg leading-relaxed">
+			Welkom bij mijn Talentenpaspoort voor PD4, hier vindt je alles wat voor PD4 ingeleverd moet
+			worden. Dit is allemaal in SvelteKit geschreven en wordt op Cloudflare gehost!
+		</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+</style>
